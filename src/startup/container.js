f9d0ca56b694944143c68429836210f1d03d159d
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-11 23:39:46 +0300

    register HomeController in awilix

--- src/startup/container.js
+++ src/startup/container.js
@@ -1,11 +1,19 @@
 const { createContainer, asClass, asValue, asFunction } = require("awilix");
 
+// service
 const { HomeService } = require("../service");
+
+// controller
+const { HomeController } = require("../controller");
 
 const container = createContainer();
 
-container.register({
-  HomeService: asClass(HomeService).singleton()
-});
+container
+  .register({
+    HomeService: asClass(HomeService).singleton()
+  })
+  .register({
+    HomeController: asClass(HomeController.bind(HomeController)).singleton()
+  });
 
 module.exports = container;
