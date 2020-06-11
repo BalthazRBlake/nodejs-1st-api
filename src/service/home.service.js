f13470980a80f2871a5f9d12886bc350631b28ea
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-11 23:11:13 +0300

    create & export HomeService class | set for injection with awilix

---
+++ src/service/home.service.js
@@ -0,0 +1,9 @@
+class HomeService {
+  index() {
+    return {
+      message: "Hello Node.js API!"
+    };
+  }
+}
+
+module.exports = HomeService;
--- src/startup/container.js
+++ src/startup/container.js
@@ -1,5 +1,11 @@
 const { createContainer, asClass, asValue, asFunction } = require("awilix");
 
+const { HomeService } = require("../service");
+
 const container = createContainer();
+
+container.register({
+  HomeService: asClass(HomeService).singleton()
+});
 
 module.exports = container;
