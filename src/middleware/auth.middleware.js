bbf85a6cdf9bfaf5a5176708f02d1462631bfdc4
FHHF <fabianhumbertohernandez@gmail.com>
2020-07-22 16:57:50 +0300

    (ADD) cache middleware & helper | set eslint-prettier to format & clean code

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,25 +1,25 @@
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../config");
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config');
 
-module.exports = function(request, response, next ) {
-  const token = request.headers["authorization"];
+module.exports = (request, response, next) => {
+  const token = request.headers.authorization;
 
   if (!token) {
     const error = new Error();
     error.status = 400;
-    error.message = "Token must be provided";
+    error.message = 'Token must be provided';
     throw error;
   }
 
-  jwt.verify(token, JWT_SECRET, function(error, decodedToken) {
-    if (error) {
+  jwt.verify(token, JWT_SECRET, (tokenError, decodedToken) => {
+    if (tokenError) {
       const error = new Error();
       error.status = 401;
-      error.message = "Invalid token";
+      error.message = 'Invalid token';
       throw error;
     }
 
     request.user = decodedToken.user;
     next();
-  })
-}
+  });
+};
